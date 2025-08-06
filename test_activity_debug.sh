3b8ee9295e8fc63ff2b634af65f4b9bb128aaf4a
Ryan <github@pocketcereal.com>
2025-08-06 13:41:44 -0700

    Remove obsolete test scripts and debug files

--- test_activity_debug.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "Testing activity indicator with debug logging..."
-echo "Try asking: 'what files are in the current directory?'"
-echo ""
-
-# Run with debug logging to see what's happening
-LOG_LEVEL=debug ./ryan
